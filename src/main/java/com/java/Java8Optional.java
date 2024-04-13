1a41ca89b70f8f3ceb8dca283e103c2ad8a418cd
ketulpatel03 <ketulpatel03@yahoo.co.in>
2024-04-13 15:04:48 -0600

    added usage example for optional

--- src/main/java/com/java/Java8Optional.java
+++ src/main/java/com/java/Java8Optional.java
@@ -2,13 +2,16 @@ package com.java;
 
 import com.java.dto.Student;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public class Java8Optional {
 
     public static void main(String[] args) {
 
-        Student student = new Student(1, "ketul", "patel", "yahoo@email.com", null);
+        Student student = new Student(1, "ketul", "patel", "yahoo@email.com", "123");
 
         // 3 ways to assign an optional Optional.empty(), of() and ofNullable()
         Optional<Object> emptyOptional = Optional.empty();
@@ -28,6 +31,23 @@ public class Java8Optional {
         // orElseThrow()
         System.out.println("orElseThrow(): " + emailOptionalOfNullable
                 .orElseThrow(() -> new IllegalArgumentException("contactNo is empty")));
+
+        // realtime usage
+        Optional<List<Student>> allStudents = getAllStudents();
+
+        if(allStudents.isPresent()){
+            allStudents.get().stream().forEach(e -> System.out.println(e.firstName()));
+        }
+    }
+
+    private static Optional<List<Student>> getAllStudents() {
+        return Optional.ofNullable(Arrays.asList(
+                new Student(1, "ketul", "patel", "yahoo@email.com", null),
+                new Student(2, "KP", "patel", "yahoo@email.com", null),
+                new Student(3, "SP", "patel", "yahoo@email.com", null),
+                new Student(4, "PP", "patel", "yahoo@email.com", null),
+                new Student(5, "MP", "patel", "yahoo@email.com", null)
+        ));
     }
 
 }
